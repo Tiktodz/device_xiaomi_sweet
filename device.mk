18b9e2c082374620b7fb4b43a2b8f61d04ea6187
Tiktodz <dotkit@electrowizard.me>
2024-10-01 17:11:23 +0000

    Revert "sweet: Build Lineage Health HAL"
    This reverts commit 60d9121d099c466ee4beb5dcbcdc4012a5eb389a.

--- device.mk
+++ device.mk
@@ -197,10 +197,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.sweet
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
